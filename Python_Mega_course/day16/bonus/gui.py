6fba5fec04ac45709a89f20c0a5d005dda71ad4c
jonito <jonathan.cattelain@gmail.com>
2022-12-18 17:15:35 +0100

    Implement the Add button

--- Python_Mega_course/day16/bonus/gui.py
+++ Python_Mega_course/day16/bonus/gui.py
@@ -1,10 +1,18 @@
-import functions
 import PySimpleGUI as sg
 
-label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
-add_button = sg.Button("Add")
+label1 = sg.Text("Select files to compress:")
+input1 = sg.Input()
+choose_button1 = sg.FilesBrowse("Choose")
 
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
+label2 = sg.Text("Select destination folder:")
+input2 = sg.Input()
+choose_button2 = sg.FolderBrowse("Choose")
+
+compress_button = sg.Button("Compress")
+
+window = sg.Window('My To-Do App', layout=[[label1, input1, choose_button1],
+                                           [label2, input2, choose_button2],
+                                           [compress_button]])
+
 window.read()
 window.close()
