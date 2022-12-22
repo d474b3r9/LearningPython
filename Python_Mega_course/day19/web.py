abdac033746488f8691983fb9b64bd741b0f1036
jonito <jonathan.cattelain@gmail.com>
2022-12-22 16:54:33 +0100

    day 19 static web interface

--- Python_Mega_course/day19/web.py
+++ Python_Mega_course/day19/web.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import functions
 # streamlit run web.py
 
+todos = functions.get_todos()
+
 st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write("This todo app is to increase productivity.")
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input("", placeholder="Add new todos ...")
